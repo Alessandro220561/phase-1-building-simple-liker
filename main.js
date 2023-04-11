c0001e85a3414811b84dc07b5cc3d9156b62ab62
Alessandro Calvera <alessandro.calvera@gmail.com>
2023-04-11 12:21:57 -0400

    complete lab

--- main.js
+++ main.js
@@ -2,16 +2,44 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+let modal = document.getElementById('modal')
+const heartIcons = [...document.getElementsByClassName('like-glyph')]
+let modalParagraph = document.getElementById('modal-message')
 
+let callServerAndCatch = (event) => {
+    mimicServerCall()
+    .then(()=> handleResponse(event))
+    .catch ((error) => handleError(error))
+}
 
+let handleError = (errorMessage) => {
+  modal.classList.remove('hidden')
+  modalParagraph.innerText = errorMessage
+  setTimeout(() => {modal.classList.add('hidden')
+  modalParagraph.innerText = ''
+  }, 3000);
+}
 
+let handleResponse = (event) => {
+  if (event.target.textContent === EMPTY_HEART){
+    event.target.classList.add('activated-heart')
+    event.target.textContent = FULL_HEART
+  } else {
+    event.target.classList.remove('activated-heart')
+    event.target.textContent= EMPTY_HEART
+  }
+}
+
+  heartIcons.map(heartIcon => {
+  heartIcon.addEventListener('click', callServerAndCatch)
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+  console.log('clicked')
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
